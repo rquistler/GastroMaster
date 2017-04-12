dceb27dd5c447ede6ade9d0dc4c4c2b8c6ee65a8
Roman <roman.quistler@hotmail.de>
2017-04-12 17:04:06 +0200

    basic setup

--- app/src/main/java/de/hsb/gastromaster/GastroMasterApp.java
+++ app/src/main/java/de/hsb/gastromaster/GastroMasterApp.java
@@ -1,7 +1,26 @@
 package de.hsb.gastromaster;
 
 import android.app.Application;
+import android.os.StrictMode;
 
 
 public class GastroMasterApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (BuildConfig.DEBUG) {
+
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .build());
+
+            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .build());
+        }
+    }
 }
