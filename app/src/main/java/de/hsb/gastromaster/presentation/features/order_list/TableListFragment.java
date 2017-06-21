1f4b1d58f837f8787c5ec8918cbf24e82311c2d9
Christian Schaf <cschaf@outlook.com>
2017-06-21 16:36:44 +0200

    Connected TableLisViewtFragment with MainActivity

--- app/src/main/java/de/hsb/gastromaster/presentation/features/order_list/TableListFragment.java
+++ app/src/main/java/de/hsb/gastromaster/presentation/features/order_list/TableListFragment.java
@@ -1,11 +1,54 @@
 package de.hsb.gastromaster.presentation.features.order_list;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
-/**
- * Created by roman on 13/06/17.
- */
+import java.util.ArrayList;
 
-public class TableListFragment extends Fragment
-        implements TableListContract.View {
+import de.hsb.gastromaster.R;
+import de.hsb.gastromaster.data.table.Table;
+
+public class TableListFragment extends Fragment implements TableListContract.View {
+    private RecyclerView tableList;
+    private RecyclerView.Adapter tableListAdapter;
+    private RecyclerView.LayoutManager tableListLayoutManager;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_table_list, container, false);
+
+        tableList = (RecyclerView) rootView.findViewById(R.id.table_list);
+        tableListLayoutManager = new LinearLayoutManager(getContext());
+        ArrayList<Table> items = new ArrayList<>();
+        items.add(Table.builder().setId(1).setName("1A").setOrderList(new ArrayList<>()).build());
+        items.add(Table.builder().setId(1).setName("2A").setOrderList(new ArrayList<>()).build());
+        items.add(Table.builder().setId(1).setName("3A").setOrderList(new ArrayList<>()).build());
+
+/*        tableListAdapter = new TableListViewAdapter(items, new BaseRecyclerViewAdapter.IOnItemClick() {
+            @Override
+            public void onClick(View view, int position) {
+                Toast.makeText(getActivity(),"Item Cllicked", Toast.LENGTH_LONG);
+            }
+        });*/
+        tableListAdapter = new TableListViewAdapterDemo(items, new TableListViewAdapterDemo.IOnItemClick() {
+            @Override
+            public void onClick(View view, int position) {
+                Toast.makeText(getActivity(), "Item clicked", Toast.LENGTH_LONG);
+            }
+        });
+
+
+        tableList.setLayoutManager(tableListLayoutManager);
+        tableList.setAdapter(tableListAdapter);
+
+        return rootView;
+    }
 }
--- app/src/main/java/de/hsb/gastromaster/presentation/ui/MainActivity.java
+++ app/src/main/java/de/hsb/gastromaster/presentation/ui/MainActivity.java
@@ -2,18 +2,32 @@ package de.hsb.gastromaster.presentation.ui;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
 import de.hsb.gastromaster.R;
+import de.hsb.gastromaster.presentation.features.order_list.TableListFragment;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    protected Fragment tableList;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+
+        tableList = new TableListFragment();
+
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragment_container, tableList, tableList.getClass().getSimpleName())
+                .addToBackStack(null).commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed();
     }
 }
 
